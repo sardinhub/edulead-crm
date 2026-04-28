95e26d444398c1acfa7359220f6ffa2b2378205e
sardinhub <sardindamis.smi@gmail.com>
2026-04-28 17:17:20 +0800

    fix: tampilkan bullet point di kolom Perhatian kwitansi

--- src/components/ReceiptModal.jsx
+++ src/components/ReceiptModal.jsx
@@ -260,7 +260,7 @@ export default function ReceiptModal({ student, onClose }) {
                 </tr>
               </tbody>
             </table>
-            <div style={{ textAlign: 'center', marginTop: '10px', fontSize: '10px', color: '#888', letterSpacing: '2px' }}>--- Register by ---</div>
+            <div style={{ textAlign: 'center', marginTop: '10px', fontSize: '10px', color: '#888', letterSpacing: '2px' }}>--- Register by: Divisi Acounting ---</div>
           </div>
         </div>
       </div>
